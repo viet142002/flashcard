4b20040f1215ad845788664de621c32e59a744f6
QuocViet <vietnguyen142002@gmail.com>
2025-12-18 11:22:48 +0700

    update content helper

--- flashcard/src/renderer/src/utils/constants/index.ts
+++ flashcard/src/renderer/src/utils/constants/index.ts
@@ -1,3 +1,2 @@
 export * from './keys'
 export * from './screens'
-export * from './controller'
--- flashcard/src/renderer/src/views/HelperView/index.tsx
+++ flashcard/src/renderer/src/views/HelperView/index.tsx
@@ -1,18 +1,92 @@
-import { CONTROLLER } from "@renderer/utils/constants";
+import { Quality } from "@renderer/utils/types"
+import { Fragment } from "react/jsx-runtime"
+
+const helpList = {
+    CONTROLLER: {
+        title: 'Controller',
+        children: {
+            NEXT: {
+                icon: 'arrow-right',
+                des: 'Từ tiếp theo',
+                cmd: 'ArrowRight',
+                key: 'next'
+            },
+            PREV: {
+                icon: 'arrow-left',
+                des: 'Quay lại từ trước',
+                cmd: 'ArrowLeft',
+                key: 'prev'
+            },
+            FLIP: {
+                icon: 'refresh',
+                des: 'Lật thé',
+                cmd: 'Space',
+                key: 'flip'
+            },
+        }
+    },
+    REVIEW: {
+        title: 'Review',
+        children: {
+            BLACKOUT: {
+                icon: 'star',
+                des: 'Hoàn toàn không nhớ',
+                cmd: String(Quality.BLACKOUT),
+                key: 'blackout'
+            },
+            INCORRECT: {
+                icon: 'star',
+                des: 'Sai, nhưng khi xem đáp án thì quen',
+                cmd: String(Quality.INCORRECT),
+                key: 'incorrect'
+            },
+            RECALL_HARD: {
+                icon: 'star',
+                des: 'Sai, nhưng gần đúng',
+                cmd: String(Quality.RECALL_HARD),
+                key: 'recallHard'
+            },
+            RECALL_GOOD: {
+                icon: 'star',
+                des: 'Đúng, nhưng khó nhớ',
+                cmd: String(Quality.RECALL_GOOD),
+                key: 'recallGood'
+            },
+            RECALL_EASY: {
+                icon: 'star',
+                des: 'Đúng, hơi phân vân',
+                cmd: String(Quality.RECALL_EASY),
+                key: 'recallEasy'
+            },
+            PERFECT: {
+                icon: 'star',
+                des: 'Đúng, dễ dàng',
+                cmd: String(Quality.PERFECT),
+                key: 'perfect'
+            },
+        }
+    }
+}
 
 export function HelperView() {
     return (
-        <div className="w-full h-full px-4 text-white">
-            <h1 className="text-center text-2xl">Helper</h1>
-            <h2 className="mt-2">Controller</h2>
-            <ul className="mt-1 ml-2 pl-2 border-l border-slate-500">
-                {Object.values(CONTROLLER).map((item) => (
-                    <li key={item.key}>
-                        <span>{item.cmd}: </span>
-                        <span>{item.des}</span>
-                    </li>
+        <div className="absolute inset-0 w-full h-full text-white grid grid-rows-[auto,1fr] pt-2">
+            <h1 className="text-center text-2xl px-4">Helper</h1>
+            <div className="overflow-auto px-4 content">
+                {Object.values(helpList).map((item) => (
+                    <Fragment key={item.title}>
+                        <h2 className="mt-2">{item.title}</h2>
+                        <ul className="mt-1 ml-2 pl-2 border-l border-slate-500">
+                            {Object.values(item.children).map((i) => (
+                                <li key={i.key}>
+                                    <span>{i.cmd}: </span>
+                                    <span>{i.des}</span>
+                                </li>
+                            ))}
+                        </ul>
+                    </Fragment>
                 ))}
-            </ul>
+            </div>
         </div>
     )
 }
